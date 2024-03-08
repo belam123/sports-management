92a89848b1bc2e90eb61fcbe23242cee5fcb54fd
belammuia <belammuia0@gmail.com>
2024-03-09 00:48:50 +0300

    :sparkles: Add controller files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::API
+    rescue_from ActiveRecord::RecordNotFound, with: :no_response
     include ActionController::Cookies
-    
+
+    def no_response
+        render json: { error: "Error" }, status: :unprocessable_entity
+      end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,28 @@
 class RegistrationsController < ApplicationController
+    def index
+        registration = Registration.all
+        render json: registration
+    end
+
+    def show
+        registration = Registration.find(params[:id])
+        render json: registration
+    end
+
+    def create
+        registration = Registration.create(registration_params)
+        if registration
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def registration_params
+
+    end
 end
