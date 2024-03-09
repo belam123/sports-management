48121ef71afa7e5b10ef515dd16e71af7fdc4474
belammuia <belammuia0@gmail.com>
2024-03-09 14:06:36 +0300

    Add files for models

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,6 @@
 class Player < ApplicationRecord
+    belongs_to :team
+    has_many :registrations
+    has_many :tournaments, through: :registrations
+    validates :name,:date_of_birth,:nationality,:position,:team_id,presence: true
 end
--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,2 +1,5 @@
 class Registration < ApplicationRecord
+    belongs_to :player
+    belongs_to :tournament
+    validates :registration_date,:registration_status,:player_id,:tournament_id,presence: true
 end
